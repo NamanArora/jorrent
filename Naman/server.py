3cc32d2b62bf22fca3e516c451fe8027f3b44029
Naman Arora <namanarora11@gmail.com>
2015-12-31 23:00:48 +0530

    Threads for sending file

--- Naman/server.py
+++ Naman/server.py
@@ -1,44 +1,50 @@
+from threading import Thread
+import Queue
 import socket
 import os
 import time
 
-SIZE=1024*2
+queue=Queue.Queue()
 IP=socket.gethostname()
+port=50001
+threads=2
+sockt=socket.socket()
+sockt.bind((IP,port))
+sockt.listen(5)
+sockt.settimeout(30)
 
-#decide the src file
-while True: 
-    src=raw_input("Enter name of file you want to send: ")
-    try:
-        f=open(src,"rb")
-        break
-    except IOError:
-        print "File not found."
-
-#create the socket
-sock=socket.socket()
-port=int(raw_input("Enter port no: "))
-sock.bind((IP,port))
-sock.listen(5)
-
-#handshake 
-c,addr=sock.accept()
-print "Connected to", addr[0]
-
-print "Sending", os.path.getsize(src)/1000 , "KB"
-
-#sending the file @1024bytes
-if SIZE:
-    start=time.clock()
-    try:
-        byte=f.read(SIZE)
-        while byte!="":
-            c.send(byte)
-            byte=f.read(SIZE)
-    finally:
-        print "File has been sent from server!"
+def send(c,addr,byte):
+        print 'Thread: ', len(byte)
+        t=queue.get()
         c.send(byte)
-        print "It took", time.clock()-start, " sec"
-    
-f.close()
-c.close()
-sock.close()
+        queue.task_done()
+
+
+def getconn():
+        c,addr=sockt.accept()
+        print "Connected to",addr[0]
+
+        src=raw_input("Enter name of file you want to send: ")
+        f=open(src,"rb")
+        size=os.path.getsize(src)/threads
+        c.send(str(size).encode())
+        print 'Sending file of size: ', size
+
+        for i in range(threads):
+            byte=f.read(size)
+            queue.put(i)
+            t=Thread(target=send,args=(c,addr,byte))
+            t.setDaemon(True)
+            t.start()
+            time.sleep(3)
+
+        queue.join()
+        f.close()
+        print "All threads died"
+        c.shutdown(socket.SHUT_RDWR)
+        c.close()
+
+try:
+    getconn()
+except socket.timeout:
+    print
