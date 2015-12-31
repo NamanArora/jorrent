9adcca1b3c184d39059501457852dbd148d304bd
Naman Arora <namanarora11@gmail.com>
2015-12-31 23:00:08 +0530

    Use threads to send file

--- Naman/client.py
+++ Naman/client.py
@@ -1,37 +1,40 @@
+from threading import Thread
+import Queue
 import socket
-import os
 import time
-
-SIZE=1024*2
+queue=Queue.Queue()
 IP=socket.gethostname()
+port=50001
+threads=2
+sockt=socket.socket()
+sockt.settimeout(30)
 
-#get the 'save as' name
-while True: 
-    dest=raw_input("Save as: ")
-    try:
-        f=open(dest,"wb")
-        break
-    except IOError:
-        print "Unable to save file."
-    
-#create the socket
-sock=socket.socket()
-port=int(input("Enter portno of server: "))
-sock.connect((IP,port))
-
-#Receiving from server @1024bytes
-if SIZE:
-    start=time.clock()
-    print "Receiving from server..."
-    byte= sock.recv(SIZE)
-    while byte!="":
+def receiver(c,size):
+        t=queue.get()
+        tempdest=dest+str(t)
+        f=open(tempdest,"wb")
+        byte= c.recv(size)
+        print 'Thread: ', len(byte)
         f.write(byte)
-        f.flush()
-        #print os.path.getsize(dest)/1024000,"MB"
-        byte= sock.recv(SIZE)    
-    print "File has been succesfully received!"
-    print "It took", time.clock()-start, " sec"
+        queue.task_done()
+        f.close()
 
-#cleaning up the shizz
-f.close()
-sock.close()
+
+sockt.connect((IP,port))
+dest=raw_input("Save as: ")
+size=int(sockt.recv(8).decode())
+
+print 'Sending file of size: ', size
+
+for i in range(threads):
+            queue.put(i)
+            t=Thread(target=receiver,args=(sockt,size))
+            t.setDaemon(True)
+            t.start()
+            time.sleep(3)
+
+queue.join()
+print "All threads died"
+
+
+
