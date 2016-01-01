689bfe4e84277f9e77cc4c54c229cfdaa5f26888
Naman Arora <namanarora11@gmail.com>
2016-01-02 03:56:29 +0530

    Major changes
    -Threads connect orderly
    -Added thread verification
    -Added a new queue to check data if sent/received
    -Proper comments
    -Exception handling
    -Better socket handling

--- Naman/client.py
+++ Naman/client.py
@@ -2,39 +2,48 @@ from threading import Thread
 import Queue
 import socket
 import time
-queue=Queue.Queue()
+
+threadcon=Queue.Queue()
+threadrcv=Queue.Queue()
+
 IP=socket.gethostname()
 port=50001
 threads=2
 sockt=socket.socket()
 sockt.settimeout(30)
 
-def receiver(c,size):
-        t=queue.get()
-        tempdest=dest+str(t)
-        f=open(tempdest,"wb")
-        byte= c.recv(size)
-        print 'Thread: ', len(byte)
-        f.write(byte)
-        queue.task_done()
-        f.close()
+def receiver(c):
+        #Get local thread id
+        t=threadcon.get()
 
+        #Get remote thread id
+        remote=c.recv(1024)
 
-sockt.connect((IP,port))
-dest=raw_input("Save as: ")
-size=int(sockt.recv(8).decode())
+        #Connected to corresponding thread
+        if t==int(remote):
+            c.send(str(t))
+            print 'Thread: ',t,'connected to: ', remote
+            threadcon.task_done()
 
-print 'Sending file of size: ', size
+        #if data has been received
+        threadrcv.task_done()
 
-for i in range(threads):
-            queue.put(i)
-            t=Thread(target=receiver,args=(sockt,size))
-            t.setDaemon(True)
-            t.start()
-            time.sleep(3)
+try:
+    sockt.connect((IP,port))
+    for i in range(threads):
+                threadcon.put(i)
+                threadrcv.put(0)
+                t=Thread(target=receiver,args=(sockt,))
+                t.setDaemon(True)
+                t.start()
+                #Wait for threads to connect
+                threadcon.join()
 
-queue.join()
-print "All threads died"
-
+    threadrcv.join()
+    print 'Data transfer complete'
+    sockt.shutdown(socket.SHUT_RDWR)
+    sockt.close()
+except socket.error:
+    print 'Could not connect to server'
 
 
