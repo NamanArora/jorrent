611b7041b6d0ccfcd33837c43d16b058ae3614e7
Naman Arora <namanarora11@gmail.com>
2016-01-02 03:55:54 +0530

    Major changes
    -Threads connect orderly
    -Added thread verification
    -Added a new queue to check data if sent/received
    -Proper comments
    -Exception handling
    -Better socket handling

--- Naman/server.py
+++ Naman/server.py
@@ -4,7 +4,11 @@ import socket
 import os
 import time
 
-queue=Queue.Queue()
+#threadcon checks if threads are connected
+#threadsent checks if data has been sent from each thread
+threadsent=Queue.Queue()
+threadcon=Queue.Queue()
+
 IP=socket.gethostname()
 port=50001
 threads=2
@@ -13,38 +17,46 @@ sockt.bind((IP,port))
 sockt.listen(5)
 sockt.settimeout(30)
 
-def send(c,addr,byte):
-        print 'Thread: ', len(byte)
-        t=queue.get()
-        c.send(byte)
-        queue.task_done()
+def send(c,addr):
+        #Set the socket to blocking mode 
+        c.setblocking(1)
+
+        #Get thread id
+        t=threadcon.get()
+
+        #Send thread id
+        c.send(str(t))
+
+        #Get remote thread id
+        remote=c.recv(1024)
+
+        #Check if connection succesful
+        if int(remote)==t:
+            threadcon.task_done()
+
+        #if data has been sent
+        threadsent.task_done()
 
 
 def getconn():
         c,addr=sockt.accept()
         print "Connected to",addr[0]
 
-        src=raw_input("Enter name of file you want to send: ")
-        f=open(src,"rb")
-        size=os.path.getsize(src)/threads
-        c.send(str(size).encode())
-        print 'Sending file of size: ', size
-
         for i in range(threads):
-            byte=f.read(size)
-            queue.put(i)
-            t=Thread(target=send,args=(c,addr,byte))
+            threadcon.put(i)
+            threadsent.put(0)
+            t=Thread(target=send,args=(c,addr))
             t.setDaemon(True)
             t.start()
-            time.sleep(3)
+            #Wait for threads to connect
+            threadcon.join()
 
-        queue.join()
-        f.close()
-        print "All threads died"
+        threadsent.join()
+        print "Data transfer complete"
         c.shutdown(socket.SHUT_RDWR)
         c.close()
 
 try:
     getconn()
 except socket.timeout:
-    print
+    print 'Server timed out'
